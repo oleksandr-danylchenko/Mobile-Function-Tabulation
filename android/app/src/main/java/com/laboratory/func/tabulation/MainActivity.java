c2d5f189a5c7c0a9a2b0c31007a801ad98124aa0
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2023-06-05 14:05:53 +0300

    Added android app setup

---
+++ android/app/src/main/java/com/laboratory/func/tabulation/MainActivity.java
@@ -0,0 +1,5 @@
+package com.laboratory.func.tabulation;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.laboratory.func.tabulation',
+  appName: 'mobile-func-tabulation',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https',
+  },
+};
+
+export default config;
