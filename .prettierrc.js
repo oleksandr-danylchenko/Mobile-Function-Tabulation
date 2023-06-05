a5cf81e1252843738f41d3ecdb982e331531bf9b
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2023-06-05 12:06:33 +0300

    Added more strict prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'preserve',
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  overrides: [
+    {
+      files: '*.html',
+      options: {
+        printWidth: 9999,
+      },
+    },
+  ],
+};
