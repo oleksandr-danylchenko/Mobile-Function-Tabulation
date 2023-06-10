0db4cdb19c48ce5f3cb5bc36b9d5685499e078f1
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2023-06-10 23:09:49 +0300

    Added chart state reset on update

--- src/components/Plot/index.tsx
+++ src/components/Plot/index.tsx
@@ -1,7 +1,7 @@
 import { FC, useCallback, useEffect, useState } from 'react';
 
 import { SerializedStyles } from '@emotion/react';
-import { Box, css, Theme } from '@mui/material';
+import { Box, css, Theme, useTheme } from '@mui/material';
 
 // import Desmos from 'desmos';
 import { MAX_DIMENSIONS_BOUNDS } from '@/constants';
@@ -10,6 +10,8 @@ import { useAppSelector } from '@/store/store';
 import { desmosButton } from '@/styles/mixins';
 
 const Plot: FC = () => {
+  const theme = useTheme();
+
   const isEvaluating = useAppSelector((state) => state.tabulation.isEvaluating);
   const results = useAppSelector((state) => state.tabulation.results);
 
@@ -28,6 +30,8 @@ const Plot: FC = () => {
           left: MAX_DIMENSIONS_BOUNDS,
           right: MAX_DIMENSIONS_BOUNDS,
         });
+        renderedCalculator.removeExpression({ id: '1' }); // Clear state entirely
+
         setCalculator(renderedCalculator);
       }
     },
@@ -38,23 +42,27 @@ const Plot: FC = () => {
     if (!calculator) return;
     if (!results || results?.evaluatedAt === prevEvaluatedAt) return;
 
-    calculator.setBlank();
+    const expressionId = 'tabulation-expression';
+    calculator.removeExpression({ id: expressionId });
     calculator.setExpression({
+      id: expressionId,
       type: 'table',
       columns: [
         {
           latex: 'x',
           values: results.x as any, // JS will auto convert to string
           dragMode: Desmos.DragModes.NONE,
+          color: theme.palette.primary.main,
         },
         {
           latex: 'y',
           values: results.y as any, // JS will auto convert to string
           dragMode: Desmos.DragModes.NONE,
+          color: theme.palette.primary.main,
         },
       ],
     });
-  }, [calculator, prevEvaluatedAt, results]);
+  }, [calculator, prevEvaluatedAt, results, theme.palette.primary.main]);
 
   return <Box ref={renderDesmos} css={desmosStyle} flex={1} />;
 };
