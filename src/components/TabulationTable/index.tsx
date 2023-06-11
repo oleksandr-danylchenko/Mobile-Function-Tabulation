818446bf212fac0170f0e622c0c8042e09ca99a4
Oleksandr Danylchenko <oleksandr.danylchenko@binary-studio.com>
2023-06-11 18:36:30 +0300

    Added virtualized table view

--- src/components/TabulationTable/index.tsx
+++ src/components/TabulationTable/index.tsx
@@ -1,178 +1,59 @@
-import { FC, ReactElement } from 'react';
-import { TableVirtuoso } from 'react-virtuoso';
+import { FC, forwardRef } from 'react';
+import { TableComponents, TableVirtuoso } from 'react-virtuoso';
 
 import {
   Stack,
-  TableContainer,
   Table,
   TableBody,
   TableCell,
+  TableContainer,
   TableHead,
   TableRow,
-  Paper,
 } from '@mui/material';
 
 import { selectZippedResults } from '@/store/slices/tabulationSlice';
 import { useAppSelector } from '@/store/store';
 
-interface RowData {
-  index: number;
-  xLabel: ReactElement;
-  x: string;
-  yLabel: ReactElement;
-  y: string;
-}
-
-// const TableComponents = {
-//   Scroller: React.forwardRef((props, ref) => <TableContainer component={Paper} {...props} ref={ref} />),
-//   Table: (props) => <Table {...props} style={{ borderCollapse: 'separate' }} />,
-//   TableHead: TableHead,
-//   TableRow: TableRow,
-//   TableBody: React.forwardRef((props, ref) => <TableBody {...props} ref={ref} />),
-// }
+const TableComponents: TableComponents<
+  ReturnType<typeof selectZippedResults>[number]
+> = {
+  Scroller: forwardRef((props, ref) => <TableContainer {...props} ref={ref} />),
+  Table: (props) => <Table {...props} style={{ borderCollapse: 'separate' }} />,
+  TableHead: TableHead,
+  TableRow: TableRow,
+  TableBody: forwardRef((props, ref) => <TableBody {...props} ref={ref} />),
+};
 
 const TabulationTable: FC = () => {
   const zippedResults = useAppSelector(selectZippedResults);
-  console.log({ zippedResults });
   return (
     <Stack flex={1} px={7} pt={2}>
-      g{/*<TableVirtuoso*/}
-      {/*  style={{ height: 400 }}*/}
-      {/*  data={zippedResults}*/}
-      {/*  components={TableComponents}*/}
-      {/*  fixedHeaderContent={() => (*/}
-      {/*    <TableRow>*/}
-      {/*      <TableCell style={{ width: 150, background: 'white' }}>*/}
-      {/*        Name*/}
-      {/*      </TableCell>*/}
-      {/*      <TableCell style={{ background: 'white' }}>*/}
-      {/*        Description*/}
-      {/*      </TableCell>*/}
-      {/*    </TableRow>*/}
-      {/*  )}*/}
-      {/*  itemContent={(index, user) => (*/}
-      {/*    <>*/}
-      {/*      <TableCell style={{ width: 150, background: 'white' }}>*/}
-      {/*        {user.name}*/}
-      {/*      </TableCell>*/}
-      {/*      <TableCell style={{ background: 'white'  }}>*/}
-      {/*        {user.description}*/}
-      {/*      </TableCell>*/}
-      {/*    </>*/}
-      {/*  )}*/}
-      {/*/>*/}
+      <TableVirtuoso
+        data={zippedResults}
+        components={TableComponents}
+        fixedHeaderContent={() => (
+          <TableRow>
+            <TableCell align="center" sx={{ backgroundColor: 'white' }}>
+              i
+            </TableCell>
+            <TableCell align="center" sx={{ backgroundColor: 'white' }}>
+              x<sub>i</sub>
+            </TableCell>
+            <TableCell align="center" sx={{ backgroundColor: 'white' }}>
+              y<sub>i</sub>
+            </TableCell>
+          </TableRow>
+        )}
+        itemContent={(index, { x, y }) => (
+          <>
+            <TableCell align="center">{index}</TableCell>
+            <TableCell align="center">{x}</TableCell>
+            <TableCell align="center">{y}</TableCell>
+          </>
+        )}
+      />
     </Stack>
   );
 };
-
-const createRowData = (index: number, x: string, y: string): RowData => ({
-  index,
-  xLabel: (
-    <>
-      x<sub>{index}</sub>
-    </>
-  ),
-  x,
-  yLabel: (
-    <>
-      y<sub>{index}</sub>
-    </>
-  ),
-  y,
-});
-
-{
-  /*<TableContainer component={Paper}>*/
-}
-{
-  /*  <Table aria-label="simple table">*/
-}
-{
-  /*    <TableHead>*/
-}
-{
-  /*      <TableRow>*/
-}
-{
-  /*        <TableCell></TableCell>*/
-}
-{
-  /*        <TableCell align="center">*/
-}
-{
-  /*          x<sub>i</sub>*/
-}
-{
-  /*        </TableCell>*/
-}
-{
-  /*        <TableCell align="center">*/
-}
-{
-  /*          y<sub>i</sub>*/
-}
-{
-  /*        </TableCell>*/
-}
-{
-  /*        <TableCell></TableCell>*/
-}
-{
-  /*      </TableRow>*/
-}
-{
-  /*    </TableHead>*/
-}
-{
-  /*    <TableBody>*/
-}
-{
-  /*      {rows.map((row) => (*/
-}
-{
-  /*        <TableRow*/
-}
-{
-  /*          key={row.name}*/
-}
-{
-  /*          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}*/
-}
-{
-  /*        >*/
-}
-{
-  /*          <TableCell component="th" scope="row" align="center">*/
-}
-{
-  /*            {row.name}*/
-}
-{
-  /*          </TableCell>*/
-}
-{
-  /*          <TableCell align="center">{row.calories}</TableCell>*/
-}
-{
-  /*          <TableCell align="center">{row.fat}</TableCell>*/
-}
-{
-  /*          <TableCell align="center">{row.carbs}</TableCell>*/
-}
-{
-  /*        </TableRow>*/
-}
-{
-  /*      ))}*/
-}
-{
-  /*    </TableBody>*/
-}
-{
-  /*  </Table>*/
-}
-{
-  /*</TableContainer> */
-}
 
 export default TabulationTable;
